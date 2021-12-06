87c9c47c4baa3fde15208d81b6a9d24fa78955a3
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-06 12:44:04 -0800

    commit tree d262fff7193dc945013d1a47a704aa86f6eca26f

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42)
+}
+//    if (blocks < 42) {
+//        return 42 - blocks;
+//    } else if (blocks > 42) {
+//        return blocks - 42;
+//    } else {
+//        return 0;
+//    }
+//}
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(pointA, pointB) {
+    return Math.abs(pointA - pointB) * 264
+}
+//    if (pointA < pointB) {
+//        return (pointB - pointA) * 264
+//    } else if (pointA > pointB) {
+//        return (pointA - pointB) * 264;
+//    }
+//}
+
+function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);  
+    if (distance <= 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return (distance - 400) * .02
+    } else if (distance >= 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
